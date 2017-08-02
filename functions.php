3c830dac5d038e07045dc2ee67cb77892de99b22
Sorwarhossain <limonhossain57@gmail.com>
2017-08-02 18:19:00 +0600

    remove woocommerce templat files for test purpose

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 // Add custom Theme Functions here
 
-remove_action( 'woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
+/*remove_action( 'woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
 
-add_action( 'custom_payment_methods_replacement', 'woocommerce_checkout_payment', 10 );
+add_action( 'custom_payment_methods_replacement', 'woocommerce_checkout_payment', 10 );*/
 
 
 
