ef127c9f7e7df18afee3aa605243d8b422ae103f
Sorwarhossain <limonhossain57@gmail.com>
2017-08-08 18:47:11 +0600

    hide go shop button when there some order in my account page

--- woocommerce/myaccount/my-orders.php
+++ woocommerce/myaccount/my-orders.php
@@ -106,13 +106,12 @@ if ( $customer_orders ) : ?>
 
 
 <?php else : ?>
-	
-
-<?php endif; ?>
-
-<div class="woocommerce-message woocommerce-message--info woocommerce-Message woocommerce-Message--info woocommerce-info">
+	<div class="woocommerce-message woocommerce-message--info woocommerce-Message woocommerce-Message--info woocommerce-info">
 		<a class="woocommerce-Button button" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
 			<?php _e( 'Go shop', 'woocommerce' ) ?>
 		</a>
 		<?php _e( 'No order has been made yet.', 'woocommerce' ); ?>
 	</div>
+<?php endif; ?>
+
+
