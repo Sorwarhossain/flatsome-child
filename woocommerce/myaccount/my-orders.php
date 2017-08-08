f32632b93c7f3819086faa8815c7a8690ecb564b
Sorwarhossain <limonhossain57@gmail.com>
2017-08-08 19:01:08 +0600

    fix no order issue my account page

--- woocommerce/myaccount/my-orders.php
+++ woocommerce/myaccount/my-orders.php
@@ -103,4 +103,16 @@ if ( $customer_orders ) : ?>
 			<?php endforeach; ?>
 		</tbody>
 	</table>
+
+
+<?php else : ?>
+	
+
 <?php endif; ?>
+
+<div class="woocommerce-message woocommerce-message--info woocommerce-Message woocommerce-Message--info woocommerce-info">
+		<a class="woocommerce-Button button" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
+			<?php _e( 'Go shop', 'woocommerce' ) ?>
+		</a>
+		<?php _e( 'No order has been made yet.', 'woocommerce' ); ?>
+	</div>
