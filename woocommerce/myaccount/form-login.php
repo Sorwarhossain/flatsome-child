5b82d1f1212ccb0e7ffd37c8cdc43e30e6357e24
Sorwarhossain <limonhossain57@gmail.com>
2017-08-23 17:07:10 +0600

    fix form recapcha error

--- woocommerce/myaccount/form-login.php
+++ woocommerce/myaccount/form-login.php
@@ -39,7 +39,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		<h3 class="uppercase"><?php _e( 'Login', 'woocommerce' ); ?></h3>
 
-		<form method="post" class="login">
+		<form method="post" class="login" action="javascript:alert(grecaptcha.getResponse(login_recapcha));">
 
 			<?php do_action( 'woocommerce_login_form_start' ); ?>
 			
@@ -84,7 +84,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		<h3 class="uppercase"><?php _e( 'Register', 'woocommerce' ); ?></h3>
 
-		<form method="post" class="register">
+		<form method="post" class="register" action="javascript:alert(grecaptcha.getResponse(register_recapcha));">
 
 			<?php do_action( 'woocommerce_register_form_start' ); ?>
 
