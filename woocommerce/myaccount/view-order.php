235a6a96e1db6b5004be3832d14009c2e6030315
Sorwarhossain <limonhossain57@gmail.com>
2017-08-30 00:19:56 +0600

    change title format

--- woocommerce/myaccount/view-order.php
+++ woocommerce/myaccount/view-order.php
@@ -54,7 +54,7 @@ if($carrier_name or $pick_up_date or $tracking_code) :
 <p class="single-order-title"><?php
 /* translators: 1: order number 2: order date 3: order status */
 printf(
-	__( '<strong>Orders And Tracking</strong> : Order # %1$s was placed on %2$s and is currently %3$s.', 'woocommerce' ),'<mark class="order-number">' . $order->get_order_number() . '</mark>',
+	__( 'Order # %1$s was placed on %2$s and is currently %3$s.', 'woocommerce' ),'<mark class="order-number">' . $order->get_order_number() . '</mark>',
 '<mark class="order-date">' . wc_format_datetime( $order->get_date_created() ) . '</mark>',
 '<mark class="order-status">' . wc_get_order_status_name( $order->get_status() ) . '</mark>');
 ?></p>
